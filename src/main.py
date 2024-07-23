51fe04d8b00f8e7a7ee9d0849ea7775a2b3ffa0e
Szymon-Budziak <szymonbudziak@gmail.com>
2024-07-23 18:55:18 +0200

    feat: yolo training notebook for players, players, referees and ball tracking

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
+from trackers import Tracker
 from utils import read_video, save_video
 
 
 def main():
     # Read video
     video_frames = read_video("input_videos/08fd33_4.mp4")
+
+    # Initialize tracker and run prediction on it
+    tracker = Tracker("model/best.pt")
+    tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path="output_videos/stub.txt")
 
     # Save video
     save_video(video_frames, "output_videos/output.avi")
